3eeb3938a4ab36bb54fbae18a2542995d7eab875
Liviu Andrei Petrache <petrache.andrei1@gmail.com>
2025-01-25 17:29:21 +0200

    fix: implemented magic check, should embed the PDF with other data

--- GenericPlugins/Dropper/src/Multimedia/PDF.cpp
+++ GenericPlugins/Dropper/src/Multimedia/PDF.cpp
@@ -2,6 +2,14 @@
 
 namespace GView::GenericPlugins::Droppper::Multimedia
 {
+/*
+* https://en.wikipedia.org/wiki/Magic_number_(programming)
+* PDF files start with "PDF-" (hex 50 44 46 2D).
+* 
+* A good example from GNU:
+* https://web.archive.org/web/20141010035745/http://gnupdf.org/Introduction_to_PDF
+*/
+constexpr uint32 PDF_MAGIC_START = 0x2D464450;
 
 const std::string_view PDF::GetName() const
 {
@@ -35,6 +43,22 @@ bool PDF::ShouldGroupInOneFile() const
 
 bool PDF::Check(uint64 offset, DataCache& file, BufferView precachedBuffer, Finding& finding)
 {
+    CHECK(IsMagicU32(precachedBuffer, PDF_MAGIC_START), false, "PDF magic not found");
+
+    finding.start = offset;
+    finding.end   = offset + sizeof(PDF_MAGIC_START) + 5;
+    auto pos      = finding.end;
+    auto found    = false;
+
+    while (true) {
+        auto buffer = file.CopyToBuffer(pos, sizeof(uint64), true);
+        CHECKBK(buffer.IsValid(), "Buffer is invalid");
+
+    }
+
+    // https://stackoverflow.com/questions/17279712/what-is-the-smallest-possible-valid-pdf
+    CHECK(finding.end - finding.start >= 70, false, "File size too small for PDF");
+    
     return true;
 }
 
